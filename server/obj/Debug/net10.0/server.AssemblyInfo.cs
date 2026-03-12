29089787e5c1497ec29ba0e7692909a0c2bde7ee
Pawel <69588920+pawel-easv@users.noreply.github.com>
2026-03-12 23:55:51 +0100

    .gitignore

--- server/obj/Debug/net10.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdbe4701e85a8071a6f987c4abdcbc707de094a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e284f9bdf0602c4658fef24e8b245924ccc340d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
